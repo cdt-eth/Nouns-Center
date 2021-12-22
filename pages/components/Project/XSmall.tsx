9a027576b300f1bfe4c6d7ea8bc68dd19035742c
christianturner <christianturner@hey.com>
2021-12-22 13:09:30 -0500

    coniditionals

--- pages/components/Project/XSmall.tsx
+++ pages/components/Project/XSmall.tsx
@@ -1,27 +1,27 @@
 import React from "react";
 
 const XSmall = ({ project }) => {
-  const { title, description, creator, url, image } = project;
+  //   const { title, description, creator, url, image } = project;
 
-  return (
-    project && (
-      <div className="">
-        <a
-          className="group block sm:cursor-pointer transition duration-200 h-full w-full aspect-w-10 aspect-h-7 rounded-xl bg-gray-100 focus-within:ring-2 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
-          href={url}
-          target="_blank"
-        >
-          <div className="absolute h-full w-full font-semibold px-4 pb-2 text-lg flex items-end gradient">
-            {title}
-          </div>
-          <img
-            className="object-cover h-full"
-            src={`/projects/${image}`}
-            alt={image}
-          />
-        </a>
-      </div>
-    )
+  return project ? (
+    <div className="">
+      <a
+        className="group block sm:cursor-pointer transition duration-200 h-full w-full aspect-w-10 aspect-h-7 rounded-xl bg-gray-100 focus-within:ring-2 relative focus-within:ring-offset-2 focus-within:ring-offset-gray-100 focus-within:ring-indigo-500 overflow-hidden"
+        href={project.url}
+        target="_blank"
+      >
+        <div className="absolute h-full w-full font-semibold px-4 pb-2 text-lg flex items-end gradient">
+          {project.title}
+        </div>
+        <img
+          className="object-cover h-full"
+          src={`/projects/${project.image}`}
+          alt={project.image}
+        />
+      </a>
+    </div>
+  ) : (
+    <></>
   );
 };
 
