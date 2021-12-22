766e09b82d0d26fcea618cc7cdc1f3184036b32a
christianturner <christianturner@hey.com>
2021-12-22 07:05:05 -0500

    mobile padding

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Navigation />
-      <div className="md:pl-72 flex flex-col flex-1 bg-black h-screen text-white">
+      <div className="xs:px-6 md:pl-72 flex flex-col flex-1 bg-black h-screen text-white">
         <main>{children}</main>
       </div>
     </>
