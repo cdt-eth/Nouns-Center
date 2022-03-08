849d264ca252209aabaf86f35e1758d485d77408
christianturner <christianturner@hey.com>
2022-03-07 22:05:27 -0500

    fix nav rules

--- pages/components/Nav.tsx
+++ pages/components/Nav.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Disclosure } from "@headlessui/react";
 import nav from "../api/nav.json";
 import Link from "next/link";
-// import Newsletter from "./newsletter";
+// import Newsletter from "./Newsletter";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
@@ -26,8 +26,9 @@ const Nav = ({ setSidebarOpen }) => {
           aria-label="Sidebar"
         >
           {/* No SubNav */}
-          {nav.map((item) =>
-            !item.children ? (
+          {nav.map(
+            (item) => (
+              // !item.children ? (
               <div key={item.name}>
                 <a
                   href={item.link}
@@ -42,88 +43,91 @@ const Nav = ({ setSidebarOpen }) => {
                   {item.name}
                 </a>
               </div>
-            ) : (
-              //  Has SubNav
-              <Disclosure as="div" key={item.name} className="space-y-1">
-                {({ open }) => (
-                  <>
-                    <Disclosure.Button
-                      className={classNames(
-                        item.current
-                          ? "bg-gray-100 text-nouns-text-nav-header focus:outline-none"
-                          : "bg-nouns-bg-darkgrey text-nouns-text-nav-header hover:bg-black  hover:text-white",
-                        "group w-full flex items-center pl-2 pr-1 py-2 text-left xs:text-text-lg sm:text-base font-medium rounded-md focus:outline-none "
-                      )}
-                    >
-                      <span className="flex-1">{item.name}</span>
-
-                      {item.children.length ? (
-                        <span
-                          className={classNames(
-                            item.current
-                              ? "bg-nouns-bg-black"
-                              : "bg-nouns-bg-black text-white group-hover:bg-gray-200",
-                            "ml-3 inline-block py-0.5 px-2 text-xs font-medium rounded-full group-hover:text-black"
-                          )}
-                        >
-                          {item.children.length}
-                        </span>
-                      ) : null}
-
-                      <svg
-                        className={classNames(
-                          open
-                            ? "text-nouns-text-nav-header rotate-90"
-                            : "text-gray-300",
-                          "ml-3 flex-shrink-0 h-5 w-5 transform group-hover:text-white transition-colors ease-in-out duration-150"
-                        )}
-                        viewBox="0 0 20 20"
-                        aria-hidden="true"
-                      >
-                        <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
-                      </svg>
-                    </Disclosure.Button>
-
-                    <Disclosure.Panel className="space-y-1">
-                      {item.children.map((subItem) => (
-                        <div
-                          key={item.name}
-                          className="flex ml-3 px-1 items-center hover:bg-black rounded-md"
-                          onClick={() => setSidebarOpen(false)}
-                        >
-                          <Link href="/">
-                            <img
-                              className="w-6 h-6 cursor-pointer rounded-full"
-                              src={`/nav/${subItem.image}`}
-                              alt="earth"
-                            />
-                          </Link>
-
-                          {subItem.external ? (
-                            <Disclosure.Button
-                              key={item.name}
-                              as="a"
-                              href={subItem.link}
-                              target="_blank"
-                              rel="no-referrer"
-                              className="focus:outline-none group w-full flex items-center pr-2 pl-2 py-1 text-base font-medium text-nouns-text-grey  hover:text-white "
-                            >
-                              {subItem.name}
-                            </Disclosure.Button>
-                          ) : (
-                            <Link href={subItem.link}>
-                              <a className="focus:outline-none group w-full flex items-center pr-2 pl-2 py-1 text-base font-medium text-nouns-text-grey  hover:text-white ">
-                                {subItem.name}
-                              </a>
-                            </Link>
-                          )}
-                        </div>
-                      ))}
-                    </Disclosure.Panel>
-                  </>
-                )}
-              </Disclosure>
             )
+
+            // {/* add back when adding full nav */}
+            // ) : (
+            //  Has SubNav
+            // <Disclosure as="div" key={item.name} className="space-y-1">
+            //   {({ open }) => (
+            //     <>
+            //       <Disclosure.Button
+            //         className={classNames(
+            //           item.current
+            //             ? "bg-gray-100 text-nouns-text-nav-header focus:outline-none"
+            //             : "bg-nouns-bg-darkgrey text-nouns-text-nav-header hover:bg-black  hover:text-white",
+            //           "group w-full flex items-center pl-2 pr-1 py-2 text-left xs:text-text-lg sm:text-base font-medium rounded-md focus:outline-none "
+            //         )}
+            //       >
+            //         <span className="flex-1">{item.name}</span>
+
+            //         {item.children.length ? (
+            //           <span
+            //             className={classNames(
+            //               item.current
+            //                 ? "bg-nouns-bg-black"
+            //                 : "bg-nouns-bg-black text-white group-hover:bg-gray-200",
+            //               "ml-3 inline-block py-0.5 px-2 text-xs font-medium rounded-full group-hover:text-black"
+            //             )}
+            //           >
+            //             {item.children.length}
+            //           </span>
+            //         ) : null}
+
+            //         <svg
+            //           className={classNames(
+            //             open
+            //               ? "text-nouns-text-nav-header rotate-90"
+            //               : "text-gray-300",
+            //             "ml-3 flex-shrink-0 h-5 w-5 transform group-hover:text-white transition-colors ease-in-out duration-150"
+            //           )}
+            //           viewBox="0 0 20 20"
+            //           aria-hidden="true"
+            //         >
+            //           <path d="M6 6L14 10L6 14V6Z" fill="currentColor" />
+            //         </svg>
+            //       </Disclosure.Button>
+
+            //       <Disclosure.Panel className="space-y-1">
+            //         {item.children.map((subItem) => (
+            //           <div
+            //             key={item.name}
+            //             className="flex ml-3 px-1 items-center hover:bg-black rounded-md"
+            //             onClick={() => setSidebarOpen(false)}
+            //           >
+            //             <Link href="/">
+            //               <img
+            //                 className="w-6 h-6 cursor-pointer rounded-full"
+            //                 src={`/nav/${subItem.image}`}
+            //                 alt="earth"
+            //               />
+            //             </Link>
+
+            //             {subItem.external ? (
+            //               <Disclosure.Button
+            //                 key={item.name}
+            //                 as="a"
+            //                 href={subItem.link}
+            //                 target="_blank"
+            //                 rel="no-referrer"
+            //                 className="focus:outline-none group w-full flex items-center pr-2 pl-2 py-1 text-base font-medium text-nouns-text-grey  hover:text-white "
+            //               >
+            //                 {subItem.name}
+            //               </Disclosure.Button>
+            //             ) : (
+            //               <Link href={subItem.link}>
+            //                 <a className="focus:outline-none group w-full flex items-center pr-2 pl-2 py-1 text-base font-medium text-nouns-text-grey  hover:text-white ">
+            //                   {subItem.name}
+            //                 </a>
+            //               </Link>
+            //             )}
+            //           </div>
+            //         ))}
+            //       </Disclosure.Panel>
+            //     </>
+            //   )}
+            // </Disclosure>
+            // )
           )}
         </nav>
         {/* <Newsletter /> */}
