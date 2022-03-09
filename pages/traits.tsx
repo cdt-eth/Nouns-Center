ec5c9d41c163c1c41dc9b0df3d91d96e898706ab
christianturner <christianturner@hey.com>
2022-03-09 11:55:49 -0500

    heads & bodies

--- pages/traits.tsx
+++ pages/traits.tsx
@@ -13,7 +13,7 @@ const Traits = () => {
       <Header title="Traits | Nouns Center" />
       <Title title="Traits" />
 
-      <div className="bg-nouns-cool py-6 xs:px-1 sm:px-4 rounded-xl">
+      <div className="bg-nouns-cool py-6 xs:px-1 sm:px-4 rounded-xl mb-8">
         <h1 className="text-5xl text-nouns mb-6 text-center text-nouns-cool-text">
           Heads
         </h1>
@@ -24,6 +24,40 @@ const Traits = () => {
                 <img src={`/traits/heads/head-${head.image}.png`} />
                 <p className="text-nouns text-nouns-cool-text text-center tracking-wider xs:text-sm sm:text-lg">
                   {head.name}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      <div className="bg-nouns-cool py-6 xs:px-1 sm:px-4 rounded-xl  mb-8">
+        <h1 className="text-5xl text-nouns mb-6 text-center text-nouns-cool-text">
+          Glasses
+        </h1>
+        <div className="grid xs:grid-cols-4 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-2 ">
+          {glasses &&
+            glasses.map((g) => (
+              <div className="flex flex-col items-center mb-8">
+                <img src={`/traits/glasses/glasses-square-${g.image}.png`} />
+                <p className="text-nouns text-nouns-cool-text text-center tracking-wider xs:text-sm sm:text-lg">
+                  {g.name}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
+
+      <div className="bg-nouns-cool py-6 xs:px-1 sm:px-4 rounded-xl  mb-8">
+        <h1 className="text-5xl text-nouns mb-6 text-center text-nouns-cool-text">
+          Bodies
+        </h1>
+        <div className="grid xs:grid-cols-4 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-2 ">
+          {bodies &&
+            bodies.map((body) => (
+              <div className="flex flex-col items-center mb-8">
+                <img src={`/traits/bodies/body-${body.image}.png`} />
+                <p className="text-nouns text-nouns-cool-text text-center tracking-wider xs:text-sm sm:text-lg">
+                  {body.name}
                 </p>
               </div>
             ))}
