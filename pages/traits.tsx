8e14f9e8a42949bc44c0586644a8941726156f31
christianturner <christianturner@hey.com>
2022-03-09 13:24:10 -0500

    traits

--- pages/traits.tsx
+++ pages/traits.tsx
@@ -63,6 +63,25 @@ const Traits = () => {
             ))}
         </div>
       </div>
+
+      <div className="bg-nouns-cool py-6 xs:px-1 sm:px-4 rounded-xl  mb-8">
+        <h1 className="text-5xl text-nouns mb-6 text-center text-nouns-cool-text">
+          Accessories
+        </h1>
+        <div className="grid xs:grid-cols-4 sm:grid-cols-5 lg:grid-cols-6 xl:grid-cols-8 gap-2 ">
+          {accessories &&
+            accessories.map((accessory) => (
+              <div className="flex flex-col items-center mb-8">
+                <img
+                  src={`/traits/accessories/accessory-${accessory.image}.png`}
+                />
+                <p className="text-nouns text-nouns-cool-text text-center tracking-wider xs:text-sm sm:text-lg">
+                  {accessory.name}
+                </p>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
