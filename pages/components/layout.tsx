5f69d62dc454da26776f9ae8d40d2385f873e985
christianturner <christianturner@hey.com>
2022-03-22 10:15:13 -0400

    paddin

--- pages/components/layout.tsx
+++ pages/components/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout({ children }) {
     <>
       <Navigation />
       <div className="xs:px-6 md:pl-[18rem] py-12 flex flex-col flex-1 h-screen bg-black text-white">
-        <main>{children}</main>
+        <main className="pb-10">{children}</main>
       </div>
     </>
   );
