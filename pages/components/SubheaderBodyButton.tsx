033880e953665ce1f36f7701721927884fddfc58
christianturner <christianturner@hey.com>
2022-03-25 12:23:37 -0400

    support custom buttons

--- pages/components/SubheaderBodyButton.tsx
+++ pages/components/SubheaderBodyButton.tsx
@@ -3,8 +3,9 @@ import React from "react";
 interface SubheaderBodyButtonProps {
   title: string;
   body: any;
-  btnLink: string;
-  btnText: string;
+  btnLink?: string;
+  btnText?: string;
+  customBtn?: any;
   download?: boolean;
 }
 
@@ -13,6 +14,7 @@ const SubheaderBodyButton = ({
   body,
   btnLink,
   btnText,
+  customBtn,
   download,
 }: SubheaderBodyButtonProps) => {
   return (
@@ -21,15 +23,19 @@ const SubheaderBodyButton = ({
         <h1 className="text-xl font-semibold capitalize">{title}</h1>
         <p className="mt-2 text-md ">{body}</p>
       </div>
-      <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-        <a
-          download={download}
-          href={btnLink}
-          target="_blank"
-          className="inline-flex capitalize items-center justify-center rounded-md border border-transparent bg-nouns-bg-blue px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 transition duration-100 focus:outline-none focus:ring-2 focus:ring-nouns-bg-blue focus:ring-offset-2 sm:w-auto"
-        >
-          {btnText}
-        </a>
+      <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none self-baseline	">
+        {customBtn ? (
+          customBtn
+        ) : (
+          <a
+            download={download && download}
+            href={btnLink && btnLink}
+            target="_blank"
+            className="inline-flex capitalize items-center justify-center rounded-md border border-transparent bg-nouns-bg-blue px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-blue-700 transition duration-100 focus:outline-none focus:ring-2 focus:ring-nouns-bg-blue focus:ring-offset-2 sm:w-auto"
+          >
+            {btnText && btnText}
+          </a>
+        )}
       </div>
     </div>
   );
