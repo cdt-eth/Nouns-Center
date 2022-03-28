d585767f8c59242a4cf7328de8d102237303e21c
christianturner <christianturner@hey.com>
2022-03-28 15:47:50 -0400

    upgrade to tailwind v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
