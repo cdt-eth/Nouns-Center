d3593d06c7ba344bc92bfdb6bcd674b40c06f2c5
christianturner <christianturner@hey.com>
2022-03-28 22:43:48 -0400

    padding

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }) {
   return (
     <>
       <Navigation />
-      <div className="xs:px-6 md:pl-[18rem] py-12 flex flex-col flex-1 flex-between h-screen bg-black text-white">
+      <div className="xs:px-6 md:pl-[18rem] xs:pt-4 xs:pb-12 sm:py-12 flex flex-col flex-1 flex-between h-screen bg-black text-white">
         <main className="pb-10">{children}</main>
 
         {asPath !== "/" && (
