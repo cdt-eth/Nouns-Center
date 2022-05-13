f7c550488dd8fef9d5c4c5b9b60e7c1e201cc287
christianturner <christianturner@hey.com>
2022-05-13 16:49:17 -0400

    root

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import Layout from "../components/Layout";
+import algoliasearch from "algoliasearch/lite";
+import { autocomplete, getAlgoliaResults } from "@algolia/autocomplete-js";
+import "@algolia/autocomplete-theme-classic";
+import { useRouter } from "next/router";
+
+// const searchClient = algoliasearch(
+//   "PGIH0KF5F5",
+//   "faf1987de1d83c02e82fbf9bfd7ca5a9"
+// );
+
+// autocomplete({
+//   container: "#autocomplete",
+//   placeholder: "Search repositories",
+// });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const { asPath } = router;
+
   return (
     <Layout>
       <Component {...pageProps} />
+      {/* {asPath === "/" && <div id="autocomplete"></div>} */}
     </Layout>
   );
 }
