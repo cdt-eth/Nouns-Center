67f7140073f2bb29756d98230f37ec3f797ff20e
christianturner <christianturner@hey.com>
2022-05-13 20:26:27 -0400

    extra home button

--- components/Home/ButtonGroup.tsx
+++ components/Home/ButtonGroup.tsx
@@ -5,9 +5,10 @@ const ButtonGroup = () => {
   return (
     <div className="mt-5 mx-auto xs:grid grid-cols-2 xs:gap-3 sm:gap-0 sm:flex sm:justify-center md:mt-8">
       <HomeButton path="projects" text="View Projects" />
-      <HomeButton path="funding" text="Get Funding" />
-      <HomeButton path="history" text="Learn the History" />
       <HomeButton path="assets" text="Download Assets" />
+      <HomeButton path="funding" text="Get Funding" />
+      <HomeButton path="dev" text="Dev Resources" />
+      <HomeButton path="history" text="History of Nouns" />
       <HomeButton path="traits" text="View Traits" />
     </div>
   );
