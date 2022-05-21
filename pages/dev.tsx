af5b524c2705f4a575748a6ebabed1e3a3d7e435
christianturner <christianturner@hey.com>
2022-05-20 22:45:33 -0400

    change dev page subheader

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -10,12 +10,7 @@ const Dev = () => {
       <Header title="Dev | Nouns Center" />
       <Title title="Dev" />
 
-      <SubheaderBodyButton
-        title="Developer Resources"
-        body="You'll find a list of technical resources & docs about working with Nouns as well as a link to our Github repos. If there's a question you have feel free to click the button and submit via Github Discussions."
-        btnLink="https://github.com/nounsDAO/nouns-monorepo/discussions/new"
-        btnText="Ask A Question"
-      />
+      <SubheaderBodyButton body="You'll find a list of technical resources & docs about working with Nouns as well as a link to our Github repos. If there's a question you have feel free to click the button and submit via Github Discussions." />
 
       <Grid />
     </div>
