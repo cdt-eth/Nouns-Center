1ca2b95b7dae1b09b32883e5e875289356c5818b
christianturner <christianturner@hey.com>
2022-05-24 09:45:05 -0400

    comment out local storage logic

--- pages/_app.js
+++ pages/_app.js
@@ -22,20 +22,20 @@ function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const { asPath } = router;
 
-  useEffect(() => {
-    if (
-      localStorage.theme === "dark" ||
-      (!("theme" in localStorage) &&
-        window.matchMedia("(prefers-color-scheme: dark)").matches)
-    ) {
-      //check if there is any key for theme in local storage and if the system color theme is dark
-      document.documentElement.classList.remove("light"); //OPTIONAL - remove light from the html document if any
-      document.documentElement.classList.add("dark"); // add dark to the <html></html> itself as <html class='dark'></html>
-    } else {
-      document.documentElement.classList.remove("dark"); // remove dark from the html document if any
-      document.documentElement.classList.add("light"); //OPTIONAL - add light to the <html></html> itself as <html class='light'></html>
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (
+  //     localStorage.theme === "dark" ||
+  //     (!("theme" in localStorage) &&
+  //       window.matchMedia("(prefers-color-scheme: dark)").matches)
+  //   ) {
+  //     //check if there is any key for theme in local storage and if the system color theme is dark
+  //     document.documentElement.classList.remove("light"); //OPTIONAL - remove light from the html document if any
+  //     document.documentElement.classList.add("dark"); // add dark to the <html></html> itself as <html class='dark'></html>
+  //   } else {
+  //     document.documentElement.classList.remove("dark"); // remove dark from the html document if any
+  //     document.documentElement.classList.add("light"); //OPTIONAL - add light to the <html></html> itself as <html class='light'></html>
+  //   }
+  // }, []);
 
   return (
     <Layout>
