53ef20fcfe22fb38fcc5c02cf839c84ad7d66e04
christianturner <christianturner@hey.com>
2022-05-24 09:42:54 -0400

    rm dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,6 @@ const { fontFamily } = require("tailwindcss/defaultTheme");
 
 module.exports = {
   mode: "jit",
-  // darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
