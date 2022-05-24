bda82bfab5cc91caf5503fb2093b3c02f714b76e
christianturner <christianturner@hey.com>
2022-05-24 09:58:31 -0400

    rm top margin

--- pages/assets.tsx
+++ pages/assets.tsx
@@ -26,7 +26,7 @@ const Assets = () => {
       <PageContent>
         {/* <div className="bg-grey-light rounded-md"> */}
         <div className="rounded-md">
-          <div className="mx-auto py-4 lg:py-8 ">
+          <div className="mx-auto">
             <ul
               role="list"
               className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-2 lg:gap-x-8 "
