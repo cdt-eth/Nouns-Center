45aa26dd325ee00bd4bef2908431b92b48d8aad0
christianturner <christianturner@hey.com>
2022-05-24 06:36:21 -0400

    new layout for assets page

--- pages/assets.tsx
+++ pages/assets.tsx
@@ -5,67 +5,75 @@ import Title from "../components/Title";
 import { v4 as uuidv4 } from "uuid";
 import assets from "../api/assets.json";
 import SubheaderBodyButton from "../components/SubheaderBodyButton";
+import PageHeader from "../components/Layout/PageHeader";
+import PageContent from "../components/Layout/PageContent";
 
 const Assets = () => {
   return (
-    <div>
-      <Header title="Assets | Nouns Center" />
-      <Title title="Assets" />
+    <>
+      <PageHeader>
+        <Header title="Assets | Nouns Center" />
+        <Title title="Assets" />
 
-      <SubheaderBodyButton
-        title="Resource Hub"
-        body="Repository of trait assets, fonts, and other resources you may need to work with Nouns. If you're still looking for something that's not here click the button and let me know."
-        btnLink="https://www.addressform.io/form/2c6a8c3e-7e17-4dfa-b30e-e0620e6773d7"
-        btnText="What's missing?"
-      />
+        <SubheaderBodyButton
+          title="Resource Hub"
+          body="Repository of trait assets, fonts, and other resources you may need to work with Nouns. If you're still looking for something that's not here click the button and let me know."
+          btnLink="https://www.addressform.io/form/2c6a8c3e-7e17-4dfa-b30e-e0620e6773d7"
+          btnText="What's missing?"
+        />
+      </PageHeader>
 
-      {/* <div className="bg-grey-light rounded-md"> */}
-      <div className="rounded-md">
-        <div className="mx-auto py-4 lg:py-8 ">
-          <ul
-            role="list"
-            className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-8 "
-          >
-            {assets.map((asset) => (
-              <li key={uuidv4()}>
-                <div className="space-y-4">
-                  <p className="dark:text-white font-medium text-lg text-nouns tracking-wide">
-                    {asset.title}
-                  </p>
-                  <div className="aspect-w-3 aspect-h-2">
-                    <img
-                      className="object-cover shadow-lg rounded-lg w-full  xs:max-h-48 sm:max-h-60 h-60"
-                      src={`/assets/${asset.image}`}
-                      alt={asset.image}
-                    />
-                  </div>
+      <PageContent>
+        {/* <div className="bg-grey-light rounded-md"> */}
+        <div className="rounded-md">
+          <div className="mx-auto py-4 lg:py-8 ">
+            <ul
+              role="list"
+              className="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-12 sm:space-y-0 lg:grid-cols-2 lg:gap-x-8 "
+            >
+              {assets.map((asset) => (
+                <li key={uuidv4()}>
+                  <div className="space-y-4">
+                    <p className="dark:text-white font-medium text-lg text-nouns tracking-wide">
+                      {asset.title}
+                    </p>
+                    <div className="aspect-w-3 aspect-h-2">
+                      <img
+                        className="object-cover shadow-lg rounded-lg w-full  xs:max-h-48 sm:max-h-60 h-60"
+                        src={`/assets/${asset.image}`}
+                        alt={asset.image}
+                      />
+                    </div>
 
-                  <div className="space-y-2">
-                    <div className="leading-6 font-medium space-y-1 flex  justify-between items-start">
-                      <a
-                        type="button"
-                        className="inline-flex sm:cursor-pointer items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md 
+                    <div className="space-y-2">
+                      <div className="leading-6 font-medium space-y-1 flex  justify-between items-start">
+                        <a
+                          type="button"
+                          className="inline-flex sm:cursor-pointer items-center px-4 py-2 shadow-sm text-sm font-medium rounded-md 
                         
                         text-black dark:text-white  bg-gray-200 focus:ring-gray-200 hover:bg-grey-base dark:bg-nouns-bg-blue dark:hover:bg-blue-700 dark:focus:ring-nouns-bg-blue 
                         "
-                        download={asset.download}
-                        href={
-                          asset.download ? `/assets/${asset.file}` : asset.file
-                        }
-                        target={asset.download ? "_self" : "_blank"}
-                        rel="noreferrer"
-                      >
-                        Download
-                      </a>
+                          download={asset.download}
+                          href={
+                            asset.download
+                              ? `/assets/${asset.file}`
+                              : asset.file
+                          }
+                          target={asset.download ? "_self" : "_blank"}
+                          rel="noreferrer"
+                        >
+                          Download
+                        </a>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </li>
-            ))}
-          </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      </div>
-    </div>
+      </PageContent>
+    </>
   );
 };
 
