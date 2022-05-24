acfbf242139146a2ee5038ba71a76430b030bb27
christianturner <christianturner@hey.com>
2022-05-24 09:28:48 -0400

    styles

--- components/Home/HomeButton.tsx
+++ components/Home/HomeButton.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import React from "react";
 import { useCallback, useEffect, useState } from "react";
 
@@ -25,10 +24,7 @@ const HomeButton = ({ path, text, buttonId }: HomeButtonProps) => {
 
   return (
     <div className="mt-3 shadow sm:mt-0 sm:ml-3">
-      {/* <Link passHref href={`/${path}`}> */}
-
       <div className="" id={"nounish_button"}>
-        {/* <div className=""> */}
         <button
           className="w-full cursor-pointer flex items-center justify-center xs:px-4 sm:px-8 py-3 border border-transparent text-base font-bold rounded-xl text-gray-800 bg-white border-[#bdc0cf] transition hover:bg-[#d5d7e1] md:py-3 xs:text-sm md:text-md tracking-normal md:px-6"
           disabled={false}
@@ -39,11 +35,6 @@ const HomeButton = ({ path, text, buttonId }: HomeButtonProps) => {
           {text}
         </button>
       </div>
-
-      {/* <span className="w-full cursor-pointer flex items-center justify-center xs:px-4 sm:px-8 py-3 border border-transparent text-base font-bold rounded-xl text-gray-800 bg-white border-[#bdc0cf] transition hover:bg-[#d5d7e1] md:py-3 xs:text-sm md:text-md tracking-normal md:px-6">
-          {text}
-        </span> */}
-      {/* </Link> */}
     </div>
   );
 };
