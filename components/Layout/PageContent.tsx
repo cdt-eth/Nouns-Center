218343d85c1b637cf04d694411872bc8718c80e8
christianturner <christianturner@hey.com>
2022-05-25 07:26:22 -0400

    responsive padding

--- components/Layout/PageContent.tsx
+++ components/Layout/PageContent.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PageContent = ({ children }) => {
   return (
-    <div className="xs:px-8 xs:py-6 sm:px-0 sm:py-12 sm:max-w-3xl m-auto bg-grey-lightest">
+    <div className="xs:px-8 xs:py-6 md:px-4 lg:px-0 sm:py-12 sm:max-w-3xl m-auto bg-grey-lightest">
       {children}
     </div>
   );
--- components/Layout/PageHeader.tsx
+++ components/Layout/PageHeader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageHeader = ({ children }) => {
   return (
     <div className="bg-grey-lightest border-b">
-      <div className="xs:py-8 xs:px-8 sm:px-0 sm:py-12 sm:pt-6 sm:max-w-3xl m-auto">
+      <div className="xs:py-8 xs:px-8 md:px-4 lg:px-0 sm:py-12 sm:pt-6 sm:max-w-3xl m-auto">
         {children}
       </div>
     </div>
