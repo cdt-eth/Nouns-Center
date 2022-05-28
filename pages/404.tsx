7010ca8ee7359b7369769dc302694ef199a45424
christianturner <christianturner@hey.com>
2022-05-27 21:13:30 -0400

    404 page layout

--- pages/404.tsx
+++ pages/404.tsx
@@ -2,18 +2,25 @@
 import React from "react";
 import Dino from "../components/Dino";
 import Header from "../components/Header";
+import PageContent from "../components/Layout/PageContent";
+import PageHeader from "../components/Layout/PageHeader";
 import Title from "../components/Title";
 
 const Custom404 = () => {
   return (
-    <div>
-      <Header title="404 | Nouns Center" />
-      <Title title="404: Page Not Found" />
-      <span className="xs:hidden sm:block">
-        <Dino />
-      </span>
-      <img className="sm:hidden" src="/dino/error.png" alt="dino" />
-    </div>
+    <>
+      <PageHeader>
+        <Header title="404 | Nouns Center" />
+        <Title title="404: Page Not Found" />
+      </PageHeader>
+
+      <PageContent>
+        <span className="xs:hidden sm:block">
+          <Dino />
+        </span>
+        <img className="sm:hidden" src="/dino/error.png" alt="dino" />
+      </PageContent>
+    </>
   );
 };
 
