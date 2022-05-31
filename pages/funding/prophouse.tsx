a0ff4dbc5fa7ac66c70f75489fb941024adb98b3
christianturner <christianturner@hey.com>
2022-05-31 05:53:24 -0400

    funding page images & link

---
+++ pages/funding/prophouse.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../../components/Header";
+import PageContent from "../../components/Layout/PageContent";
+import PageHeader from "../../components/Layout/PageHeader";
+import Title from "../../components/Title";
+
+const prophouse = () => {
+  return (
+    <>
+      <PageHeader>
+        <Header title="Prop House | Nouns Center" />
+        <Title title="Funding: Prop House" />
+      </PageHeader>
+
+      <PageContent>
+        <div>
+          <div></div>
+        </div>
+      </PageContent>
+    </>
+  );
+};
+
+export default prophouse;
---
+++ pages/funding/smallgrants.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../../components/Header";
+import PageContent from "../../components/Layout/PageContent";
+import PageHeader from "../../components/Layout/PageHeader";
+import Title from "../../components/Title";
+
+const smallgrants = () => {
+  return (
+    <>
+      <PageHeader>
+        <Header title="Small Grants | Nouns Center" />
+        <Title title="Funding: Small Grants" />
+      </PageHeader>
+
+      <PageContent>
+        <div>
+          <div></div>
+        </div>
+      </PageContent>
+    </>
+  );
+};
+
+export default smallgrants;
