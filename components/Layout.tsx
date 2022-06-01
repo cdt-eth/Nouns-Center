25e5e4ec7686edc5cadd4cd27cd31514ac571040
christianturner <christianturner@hey.com>
2022-05-31 23:40:09 -0400

    rm import

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,43 +1,12 @@
 import Navigation from "./Navigation";
-import { useRouter } from "next/router";
 
 export default function Layout({ children }) {
-  const router = useRouter();
-  const { asPath } = router;
-
   return (
     <>
       <Navigation />
 
       <div className=" flex flex-col flex-1 flex-between h-auto text-black m-auto">
-        <main className="pb-10 w-full DocSearch-content">
-          {asPath === "/" && (
-            <>
-              <video
-                id="background-video"
-                src="/nounish.mp4"
-                className="xs:hidden sm:block"
-                autoPlay
-                loop
-                webkit-playsinline="true"
-                playsInline
-                muted
-              />
-              {/* eslint-disable-next-line @next/next/no-img-element */}
-              {/* <img
-                className="sm:hidden nounish"
-                src="/nnsh.gif"
-                alt="nounish"
-              /> */}
-
-              <div className="bg-gradient"></div>
-              <div className="bg-gradient2"></div>
-              <div className="bg-gradient3"></div>
-            </>
-          )}
-
-          {children}
-        </main>
+        <main className="w-full DocSearch-content">{children}</main>
       </div>
     </>
   );
