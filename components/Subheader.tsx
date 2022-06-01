1599ec27a67f89f592298cb70924e21cd454bf85
christianturner <christianturner@hey.com>
2022-05-31 23:38:04 -0400

    optional body in subheader

--- components/Subheader.tsx
+++ components/Subheader.tsx
@@ -2,15 +2,17 @@ import React from "react";
 
 interface SubheaderProps {
   title?: string;
-  body: any;
+  body?: string | any;
 }
 
 const Subheader = ({ title, body }: SubheaderProps) => {
   return (
     <div className="sm:flex sm:items-center ">
-      <div className="sm:flex-auto">
+      <div className="sm:flex-auto mb-4">
         {title && <h1 className="text-xl font-semibold capitalize">{title}</h1>}
-        <p className={`mb-4 text-md font-medium ${title && "mt-4"}`}>{body}</p>
+        {body && (
+          <p className={`text-md font-medium ${title && "mt-4"}`}>{body}</p>
+        )}
       </div>
     </div>
   );
