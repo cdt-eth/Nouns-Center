a2334ed8e29c7ea7f310587d13591a69c9795f17
christianturner <christianturner@hey.com>
2022-06-06 09:52:40 -0400

    talent page

--- pages/talent.tsx
+++ pages/talent.tsx
@@ -36,7 +36,7 @@ const Talent = () => {
               className="col-span-1  bg-white rounded-lg shadow-md divide-y divide-gray-200 flex flex-col justify-between"
             >
               <div className="w-full flex items-center justify-between p-6 space-x-6 ">
-                <div className="flex-1 ">
+                <div className="flex-1">
                   <div className="flex items-center space-x-3">
                     <h3 className="text-gray-900 text-sm font-medium text-nouns tracking-wider">
                       {person.name !== "" ? person.name : "-"}
@@ -66,6 +66,7 @@ const Talent = () => {
                   alt=""
                 />
               </div>
+
               {(person.twitter || person.discord) && (
                 <div>
                   <div className="-mt-px flex divide-x divide-gray-200 bg-gray-100 rounded-b-lg	">
