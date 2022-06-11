fc3fbb57675983fb5a02594df3b80b308e1c42a5
Steve Finkelstein <sf@stevefink.net>
2022-06-11 12:11:35 -0400

    first pass of ideas feature
    Signed-off-by: Steve Finkelstein <sf@stevefink.net>

---
+++ pages/api/ideas.ts
@@ -0,0 +1,51 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { verifyToken } from '../../lib/utils';
+
+import { insertIdea, getIdeas } from '../../lib/db/hasura';
+
+export default async function ideas(req: NextApiRequest, res: NextApiResponse) {
+  const { method } = req;
+  if (method == 'POST') {
+    try {
+      const token = req.cookies.nc;
+      if (!token) {
+        res.status(403).send({});
+      } else {
+        const { title, tldr, description } = req.body;
+        if (
+          title?.length >= 5 &&
+          tldr?.length >= 5 &&
+          description?.length >= 5
+        ) {
+          const address = await verifyToken(token);
+          // insert idea
+          const response = await insertIdea(token, {
+            address,
+            title,
+            tldr,
+            description,
+          });
+
+          if (response?.data?.insert_ideas_one) {
+            const createdData = response?.data?.insert_ideas_one;
+            res.send({ success: true, data: createdData });
+          } else {
+            res.status(500).send({});
+          }
+        } else {
+          res.status(403).send({});
+        }
+      }
+    } catch (error) {
+      console.error('Error occurred in /ideas', error);
+      res.status(500).send({ done: false, error: error?.message });
+    }
+  } else if (method == 'GET') {
+    // const response = await getIdeas();
+    // console.log('response: ', response);
+    // res.send({ success: true, data: response });
+  } else {
+    res.setHeader('Allow', ['POST']);
+    res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
---
+++ pages/ideas/[ideaId].tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import Link from 'next/link';
+
+import Header from '../../components/Header';
+import PageHeader from '../../components/Layout/PageHeader';
+import PageContent from '../../components/Layout/PageContent';
+import Title from '../../components/Title';
+import { getIdea } from '../../lib/db/hasura';
+import { truncateEthAddress } from '../../lib/utils';
+
+import { useEnsName } from 'wagmi';
+
+export async function getServerSideProps(context) {
+  const ideaId = context.params.ideaId;
+  const ideas = await getIdea(ideaId);
+  if (!ideas.length) {
+    return {
+      notFound: true,
+    };
+  }
+
+  return {
+    props: {
+      idea: ideas[0],
+    },
+  };
+}
+const Idea = ({ idea }) => {
+  const { id, title, tldr, description, created_at, address } = idea;
+  const { data: ensName } = useEnsName({ address: address });
+
+  return (
+    <>
+      <PageHeader>
+        <Header title="Ideas | Nouns Center" />
+        <Title title="Ideas" />
+      </PageHeader>
+
+      <PageContent>
+        <Link href={`/ideas`}>
+          <a className="inline-flex cursor-pointer items-center font-medium transition rounded-md text-blue-500 hover:text-gray-700 underline">
+            Go back
+          </a>
+        </Link>
+        <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+          <div className="px-4 py-5 sm:px-6">
+            <h3 className="text-lg leading-6 font-medium text-gray-900">
+              {title}
+            </h3>
+            <p className="mt-1 max-w-2xl text-sm text-gray-500">
+              [Idea #{id}] Submitted by:{' '}
+              {ensName ? ensName : truncateEthAddress(address)}
+            </p>
+          </div>
+          <div className="border-t border-gray-200">
+            <dl>
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">tldr</dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {tldr}
+                </dd>
+              </div>
+
+              <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Description
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  <ReactMarkdown className="mt-5 text-2xl">
+                    {description}
+                  </ReactMarkdown>
+                </dd>
+              </div>
+            </dl>
+          </div>
+        </div>
+      </PageContent>
+    </>
+  );
+};
+
+export default Idea;
