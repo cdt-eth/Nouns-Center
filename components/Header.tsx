2d699b81e0fe73424b2f39a2c8477085776708bc
christianturner <christianturner@hey.com>
2022-07-26 13:05:43 -0400

    add static OpenGraph image

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,8 +8,38 @@ interface HeaderProps {
 const Header = ({ title }: HeaderProps) => {
   return (
     <Head>
-      <title>{title}</title>
+      <title>{title || "Nouns Center"}</title>
       <link rel="icon" href="/favicon.ico" />
+
+      {/* Primary Meta Tags */}
+      <meta name="title" content="Nouns Center" />
+      <meta
+        name="description"
+        content="The knowledge center and resource hub for Nouns DAO. Learn about the project, the community and ways to get involved and funded."
+      />
+
+      {/* Open Graph / Facebook */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://nouns.center/" />
+      <meta property="og:title" content="Nouns Center" />
+      <meta
+        property="og:description"
+        content="The knowledge center and resource hub for Nouns DAO. Learn about the project, the community and ways to get involved and funded."
+      />
+      <meta property="og:image" content="%PUBLIC_URL%/og-image.png" />
+
+      {/* Twitter */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://nouns.center/" />
+      <meta property="twitter:title" content="Nouns Center" />
+      <meta
+        property="twitter:description"
+        content="The knowledge center and resource hub for Nouns DAO. Learn about the project, the community and ways to get involved and funded."
+      />
+      <meta
+        property="twitter:image"
+        content="https://nouns.center/og-image.png"
+      />
     </Head>
   );
 };
