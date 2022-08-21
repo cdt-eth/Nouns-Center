7059a2b91f48c24564035942591010d6fb3c1c21
pbrianandj <bcj26@case.edu>
2022-08-20 18:27:34 -0700

    remove client side image url validation

--- utils/project-form-data-fetching.ts
+++ utils/project-form-data-fetching.ts
@@ -6,33 +6,11 @@ export const PROJECT_FORM_ID = "9e6bc6c2-0f0d-4420-b66e-0d416a5fe73a";
  * Fetch talent from data from AddressForm API
  */
 export const fetchProjectFormData = async () => {
-  return await Promise.all(postProcessProjectApiResponse(await fetchRawFormResponse(PROJECT_FORM_ID)));
+  return postProcessProjectApiResponse(await fetchRawFormResponse(PROJECT_FORM_ID));
 };
 
-async function checkImage(url){
-    try {
-  const res = await fetch(url);
-  const buff = await res.blob();
- 
-  return buff.type.startsWith('image/')
-    } catch (e){
-      console.log(e);
-      return false;
-    }
-}
-
-const validateOrReplaceImageURL = async (imageUrl: string | undefined) => {
-  if (imageUrl === undefined) {
-    return "earth.gif";
-  }
-
-  const isValid = await checkImage(imageUrl);
-  if (isValid) {
-    return imageUrl;
-  }
-
-  return "earth.gif";
-
+const validateOrReplaceImageURL = (imageUrl: string | undefined) => {
+  return imageUrl === undefined ? "earth.gif" : imageUrl
 }
 
 const postProcessProjectApiResponse = (rawProjectResponse: AddressFormAPIResponse) => {
@@ -43,7 +21,7 @@ const postProcessProjectApiResponse = (rawProjectResponse: AddressFormAPIRespons
         descrtipion: fetchFieldByString(responseData, "description"),
         creator: fetchFieldByString(responseData, "Creator(s)"),
         url: fetchFieldByString(responseData, "link"),
-        image: await validateOrReplaceImageURL(fetchFieldByString(responseData, "Thumbnail")),
+        image: validateOrReplaceImageURL(fetchFieldByString(responseData, "Thumbnail")),
         category: fetchFieldByString(responseData, "categories")
       }
     });
