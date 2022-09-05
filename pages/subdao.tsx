fd23656b13195d794a1a523ab93a0c1d36c56063
christianturner <christianturner@hey.com>
2022-09-05 06:50:50 -0400

    more subdao page changes

--- pages/subdao.tsx
+++ pages/subdao.tsx
@@ -5,10 +5,14 @@ import Title from "../components/Title";
 import Subheader from "../components/Subheader";
 import PageHeader from "../components/Layout/PageHeader";
 import PageContent from "../components/Layout/PageContent";
-import Button from "../components/common/Button";
 import subdaos from "../api/subdaos.json";
-import ReactMarkdown from "react-markdown";
 import Markdown from "markdown-to-jsx";
+import {
+  FaTwitter as TwitterIcon,
+  FaDiscord as DiscordIcon,
+  FaDiscourse as DiscourseIcon,
+} from "react-icons/fa";
+import { TbWorld as WebsiteIcon } from "react-icons/tb";
 
 const Subdao = () => {
   function classNames(...classes) {
@@ -23,11 +27,18 @@ const Subdao = () => {
     <a {...props}>{children}</a>
   );
 
+  const stats = [
+    { label: "Founded", value: "2021" },
+    { label: "Members", value: "592" },
+    { label: "Treasury", value: "Ξ 250" },
+    { label: "Proposals", value: "3" },
+  ];
+
   interface SubdaoProps {
     name: "string";
     overview: "string";
     join: "string";
-    links: [platform: "string", link: "string", img: "string"];
+    links: [platform: "string", link: "string"];
   }
 
   return (
@@ -43,9 +54,31 @@ const Subdao = () => {
         <div className="subdaos">
           {subdaos.map((s: any) => (
             <div key={s.description} className="pb-8">
-              <p className="font-bold text-2xl tracking-wider underline text-nouns">
-                {s.name}
-              </p>
+              <div className="flex items-center gap-3">
+                <p className="font-bold text-3xl tracking-wider text-nouns">
+                  {s.name}
+                </p>
+                <p className="font-bold text-2xl">{" • "}</p>
+
+                <div className="flex gap-3 ">
+                  {s.links.map((link) => (
+                    <div key={link.link}>
+                      <a
+                        className="w-12  "
+                        href={link.link}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {link.platform === "Website" && <WebsiteIcon />}{" "}
+                        {link.platform === "Twitter" && <TwitterIcon />}{" "}
+                        {link.platform === "Discord" && <DiscordIcon />}{" "}
+                        {link.platform === "Discourse" && <DiscourseIcon />}{" "}
+                      </a>
+                    </div>
+                  ))}
+                </div>
+              </div>
+              <div className="border-t-2 "></div>
               <div className="rounded-xl py-4">
                 <img className="" src={s.img} alt={s.img} />
               </div>
@@ -84,15 +117,22 @@ const Subdao = () => {
                 </Markdown>
               </div>
 
-              <div className="pb-4">
-                <p className="font-bold">Useful Links:</p>
-                {s.links.map((link) => (
-                  <div key={link.link}>
-                    <a href={link.link} target="_blank" rel="noreferrer">
-                      {link.platform}{" "}
-                    </a>
-                  </div>
-                ))}
+              <div className="mb-20 flex">
+                <dl className="grid grid-cols-2 gap-x-4 gap-y-4 w-1/3">
+                  {stats.map((stat) => (
+                    <div
+                      key={stat.label}
+                      className="border-t-2 border-gray-100 pt-6"
+                    >
+                      <dt className="text-base font-medium text-gray-500">
+                        {stat.label}
+                      </dt>
+                      <dd className="text-2xl font-bold tracking-tight text-gray-900">
+                        {stat.value}
+                      </dd>
+                    </div>
+                  ))}
+                </dl>
               </div>
             </div>
           ))}
