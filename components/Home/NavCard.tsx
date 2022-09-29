5aa67abe20d2d7861ed36eff15f7c351e0692580
ryeshrimp <ryeshrimp@protonmail.com>
2022-09-29 10:34:58 -0400

    Updates nav card to allow command + click

--- components/Home/NavCard.tsx
+++ components/Home/NavCard.tsx
@@ -9,15 +9,14 @@ export const NavCard: React.FC<{
 }> = ({ text, path, i }) => {
   return (
     <Link passHref href={`${path}`}>
-      <div className="hover:scale-[1.02] transition">
+      <a className="hover:scale-[1.02] transition" href={`${path}`}>
         <button className="text-nouns rounded-xl sm:cursor-pointer xs:h-32 xs:w-32 sm:h-60 sm:w-60 bg-gray-100 relative overflow-hidden m-auto">
           <div className="absolute bottom-0 w-full px-4 py-1 justify-center text-sm sm:text-lg flex items-end font-light text-white shadow-lg bg-black">
             {text}
           </div>
-
           <img className="object-cover w-full h-full" src={getRandomNounPic(i)} alt={text} />
         </button>
-      </div>
+      </a>
     </Link>
   );
 };
