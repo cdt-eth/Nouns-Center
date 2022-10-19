1c4ae73ff986ab11b5d38399a049001400233637
christianturner <christianturner@hey.com>
2022-10-19 10:28:46 -0400

    add loading noun pic, add Topics

--- components/groups/Table.tsx
+++ components/groups/Table.tsx
@@ -5,12 +5,13 @@ import { FaTwitter as TwitterIcon, FaDiscord as DiscordIcon } from 'react-icons/
 interface GroupsProps {
   id: string;
   Name: string;
-  'Discord Invite': string;
+  Discord: string;
   'Leading people': string[];
   Platform: string[];
   Description?: string;
   Twitter?: string;
   Logo: any;
+  Topics: string[];
 }
 
 interface Groups {
@@ -30,8 +31,8 @@ const Table = ({ groups }: Groups) => {
   // };
 
   // const getNumberOfDiscordMembers = (group: GroupsProps) => {
-  //   if (!group['Discord Invite']) return '-';
-  //   const inviteID = group['Discord Invite'].split('gg/')[1];
+  //   if (!group.Discord) return '-';
+  //   const inviteID = group.Discord.split('gg/')[1];
 
   //   const memberCount = fetchMemberCount(inviteID);
 
@@ -41,20 +42,7 @@ const Table = ({ groups }: Groups) => {
   return (
     <div className="px-4 sm:px-6 lg:px-8 ">
       <div className="sm:flex sm:items-center">
-        <div className="sm:flex-auto">
-          {/* <h1 className="text-xl font-semibold text-gray-900">Nounish Groups & Communities</h1> */}
-          {/* <p className="mt-2 text-sm text-gray-700">
-            A list of all the groups in the Nouns universe including their description and Discord invite link. Don't see your group here? Add it via the button, and we'll get it up as soon as possbile.
-          </p> */}
-        </div>
-        {/* <div className="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-          <button
-            type="button"
-            className="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto"
-          >
-            Add your group
-          </button>
-        </div> */}
+        <div className="sm:flex-auto"></div>
       </div>
 
       <div className="mt-8 flex flex-col">
@@ -78,7 +66,7 @@ const Table = ({ groups }: Groups) => {
                     </th> */}
                     <th
                       scope="col"
-                      className="px-12 py-3.5 text-center text-sm font-semibold text-gray-900"
+                      className="px-4 sm:px-12 py-3.5 text-center text-sm font-semibold text-gray-900"
                     >
                       Join
                     </th>
@@ -93,23 +81,39 @@ const Table = ({ groups }: Groups) => {
 
                 <tbody className="divide-y divide-gray-200 bg-white">
                   {groups.map(group => {
+                    console.log('g', group);
                     return (
                       <tr key={group.id}>
                         <td className=" py-4 pl-4 pr-3 text-sm sm:pl-6">
                           <div className="flex items-center">
-                            <div className="h-14 w-14 flex-shrink-0">
+                            <div className="h-14 w-14 flex-shrink-0 xs:hidden sm:flex">
                               <img
                                 className="h-14 w-14 rounded-full"
-                                src={group.Logo ? group.Logo[0].url : ''}
+                                src={group.Logo ? group.Logo[0].url : './loadingnoun.jpg'}
                                 alt={group.Logo ? group.Logo[0].name : ''}
                               />
                             </div>
 
-                            <div className="ml-4">
-                              <div className="font-medium text-nouns tracking-wider text-gray-900">
-                                {group.Name}
+                            <div className="sm:ml-4">
+                              <div className="flex gap-4">
+                                <div className="font-medium text-nouns tracking-wider text-gray-900">
+                                  {group.Name ? group.Name : '-'}
+                                </div>
+                                <div className="font-medium text-nouns tracking-wider text-gray-900">
+                                  {group.Topics.map(topic => (
+                                    <span
+                                      key={topic}
+                                      className="flex-shrink-0 ml-1.5 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full lowercase text"
+                                    >
+                                      {topic}
+                                    </span>
+                                  ))}
+                                </div>
                               </div>
-                              <div className="text-gray-500">{group.Description}</div>
+
+                              <div className="text-gray-500">
+                                {group.Description ? group.Description : '-'}
+                              </div>
                             </div>
                           </div>
                         </td>
@@ -120,11 +124,11 @@ const Table = ({ groups }: Groups) => {
                           </div>
                         </td> */}
 
-                        <td className=" px-12 py-4 text-center text-sm text-blue-base groupsDiscord">
-                          {group['Discord Invite'] ? (
+                        <td className="px-4 sm:px-12 py-4 text-center text-sm text-blue-base groupsDiscord">
+                          {group.Discord ? (
                             <>
                               <a
-                                href={group['Discord Invite']}
+                                href={group.Discord}
                                 target="_blank"
                                 rel="noreferrer"
                                 className="hover:opacity-70 transition duration-100"
